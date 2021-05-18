629d42ee00b99579af360b7f498d0eb43f5411d7
mogstuff <markmorgan.mcp@gmail.com>
2021-05-18 10:20:17 +0100

    feat: basic form validation
    first pass at form validation

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,16 +7,29 @@ function handleSubmit(e){
         
         let searchText = document.getElementById('search_txt').value;
 
+        if(searchText.length < 3){
+            throw "search cannot be null";
+        }
+
         console.log('search for: ' + searchText);
 
-
-
     } catch (error) {
-        
+        displayErrors(error);
     }
-
 
 }
 
 
-export { handleSubmit }
+function displayErrors(error) {
+   
+    let errors = document.getElementById('validation');
+    errors.innerHTML = '';
+    let p = document.createElement('p');
+    p.classList.add('error');
+    p.innerText = error;
+    errors.appendChild(p);
+}
+
+
+
+export { handleSubmit, displayErrors }
