b7eb21b6a7de6f04e7fb83cb3d2c495bfdcdac80
mogstuff <markmorgan.mcp@gmail.com>
2021-05-23 17:28:09 +0100

    feat: service workers
    added service workers for offline access

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const WorkboxPlugin = require('workbox-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
